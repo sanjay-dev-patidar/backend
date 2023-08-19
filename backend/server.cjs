a6804f6103311a07536219d9de25be8f6043aea5
sanjay-dev-patidar <142602180+sanjay-dev-patidar@users.noreply.github.com>
2023-08-20 01:33:03 +0530

    Update server.cjs

--- backend/server.cjs
+++ backend/server.cjs
@@ -58,6 +58,7 @@ const Tools = mongoose.model('tools', {
   description: [String],
   keypoints: [String],
   imageURL: [String],
+  videoURL: [String],
 });
 const Working = mongoose.model('working', {
   title: String,
